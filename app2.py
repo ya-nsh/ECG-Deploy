a65ddf11d0142093d8b492d2b1c966f4b00da30e
ya-nsh <yansh2017@gmail.com>
2022-11-18 17:34:19 +0530

    add images and change code

--- app2.py
+++ app2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36f31ca6fd1ea7725e47cb82f58e4a1b62a321d7fba372c48b9ee43459e17227
-size 4349
+oid sha256:ffb829ffc21d66c698a3bbc6b818dc55462ba09df26176250f6f9d6c1bc3148a
+size 4470
